cf991c25b201f8e1c175d8f10edb6d510c54a7b2
Alvaro <alvaro.camilloneto@gmail.com>
2019-10-19 00:40:20 -0300

    Criação original do projeto

---
+++ alurapic/src/app/photos/photo-list/photos/photos.component.ts
@@ -0,0 +1,32 @@
+import { Photo } from './../../photo/photo.model';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-photos',
+  templateUrl: './photos.component.html',
+  styleUrls: ['./photos.component.css']
+})
+export class PhotosComponent implements OnChanges {
+  @Input()
+  photos: Photo[];
+
+  rows: any[] = [];
+
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.photos) {
+      this.rows = this.groupColumns(this.photos);
+    }
+  }
+
+  private groupColumns(photos: Photo[]) {
+    const newRows = [];
+
+    for (let index = 0; index < photos.length; index += 3) {
+      newRows.push(photos.slice(index, index + 3));
+    }
+
+    return newRows;
+  }
+}
