7e7f3ba4cd1c2c0a76d64bc6acb8bd8581d5701c
Alvaro Camillo Neto <alvaro.camillo@totvs.com.br>
2019-11-26 01:19:26 -0300

    Incluido teste do componente

---
+++ alurapic/src/app/photos/photo-list/search/search.component.spec.ts
@@ -0,0 +1,23 @@
+import { SearchComponent } from "./search.component";
+import { ComponentFixture, async, TestBed } from "@angular/core/testing";
+
+describe("SearchComponent", () => {
+  let component: SearchComponent;
+  let fixture: ComponentFixture<SearchComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [SearchComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
