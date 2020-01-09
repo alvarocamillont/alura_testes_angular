c6e580b42f26a968f6900144098cf34b41c50e44
Alvaro <alvaro.camilloneto@gmail.com>
2020-01-09 01:56:58 -0300

    Corrigido teste

--- alurapic/src/app/core/header/header.component.spec.ts
+++ alurapic/src/app/core/header/header.component.spec.ts
@@ -1,53 +1,58 @@
-import { ComponentFixture, async, TestBed } from "@angular/core/testing";
-import { User } from "../user/user";
-import { UserService } from "../user/user.service";
-import { of } from "rxjs";
-import { RouterTestingModule } from "@angular/router/testing";
-import { HeaderComponent } from "./header.component";
-import { Router } from "@angular/router";
-import { MenuModule } from "src/app/shared/componets/menu/menu.module";
-import { LoadingModule } from "src/app/shared/componets/loading/loading.module";
-import { AlertModule } from "src/app/shared/componets/alert/alert.module";
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { AlertModule } from 'src/app/shared/componets/alert/alert.module';
+import { LoadingModule } from 'src/app/shared/componets/loading/loading.module';
+import { MenuModule } from 'src/app/shared/componets/menu/menu.module';
 
-describe('HeaderComponent', () => {
+import { User } from '../user/user';
+import { UserService } from '../user/user.service';
+import { HeaderComponent } from './header.component';
+
+describe("HeaderComponent", () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
   let fakeUser: User;
-  let userService:UserService;
-  let router:Router;
+  let userService: UserService;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [HeaderComponent],
       providers: [UserService],
-      imports:[RouterTestingModule.withRoutes([]),MenuModule,LoadingModule,AlertModule]
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        MenuModule,
+        LoadingModule,
+        AlertModule
+      ]
     }).compileComponents();
-
-    fakeUser = {
-      email:'alvaro@alvaro.com',
-      id:1,
-      name:'Alvaro'
-    }
-    userService = TestBed.get(UserService);
-    router =TestBed.get(Router);
-    spyOn(userService,'getUser').and.returnValue(of(fakeUser));
   }));
 
   beforeEach(() => {
+    fakeUser = {
+      email: "alvaro@alvaro.com",
+      id: 1,
+      name: "Alvaro"
+    };
+    userService = TestBed.get(UserService);
+    router = TestBed.get(Router);
+    spyOn(userService, "getUser").and.returnValue(of(fakeUser));
+
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('deve ser criado', () => {
+  it("deve ser criado", () => {
     expect(component).toBeTruthy();
   });
 
-  it('deve realizar o logout', () => {
-    const navigateSpy = spyOn(router,'navigate');
-    spyOn(userService,'logout').and.returnValue(null);
+  it("deve realizar o logout", () => {
+    const navigateSpy = spyOn(router, "navigate");
+    spyOn(userService, "logout").and.returnValue(null);
     component.logout();
-    expect(navigateSpy).toHaveBeenCalledWith(['']);
+    expect(navigateSpy).toHaveBeenCalledWith([""]);
   });
-
 });
